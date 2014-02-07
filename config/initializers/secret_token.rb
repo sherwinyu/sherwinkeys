05d857e84923406eb4fd24759381d069231c694c
Sherwin Yu <sherwin.x.yu@gmail.com>
2014-02-07 16:25:25 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sherwinkeys::Application.config.secret_token = '61e085fbd4022bab1338b7c73708364373e3c6930d6b8bbb72c849abc15be37fa091c956476a40abe74b6250e99c682618629c89e9926355359b38e94f22d0b6'
